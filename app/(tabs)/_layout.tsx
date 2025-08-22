a262af93d99d2cc19175495c08d5a57fb5c00db2
maeta187 <nexus18789@gmail.com>
2025-08-22 17:14:19 +0900

    ✨ : FlexLayoutの学習

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -126,6 +126,15 @@ export default function TabLayout() {
           )
         }}
       />
+      <Tabs.Screen
+        name='flex-basis-grow-shrink'
+        options={{
+          title: 'flexBasisGrowShrink',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name='7.circle' color={color} />
+          )
+        }}
+      />
     </Tabs>
   )
 }
